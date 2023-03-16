ce97cba3246b1ac11ce8581476cdcad0ef94abd4
YOUSSEF-YL <youssef.bachar7@gmail.com>
2023-03-16 23:02:14 +0100

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main- just test
+ * Return: comment for betty
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
